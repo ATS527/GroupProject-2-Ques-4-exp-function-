a26284afd2d10397d73c55b58224b86c20898499
ATS527 <63837472+ATS527@users.noreply.github.com>
2020-11-09 11:41:23 +0530

    Update main.cpp
    Added comments to provide better readability

--- main.cpp
+++ main.cpp
@@ -4,25 +4,25 @@ using namespace std;
 int main()
 {
 	double x;
-  double exponent;
-	double fact {1};	
+  	double exponent;  //answer from equation
+	double fact {1};	//factorial
 	char choice;
 	do
 	{
-    cout << "Enter the value of x : ";
-    cin >> x;
-    cout << "\nValue of e^x using exp() built in function : " << exp(x) << endl;
+    	cout << "Enter the value of x : ";
+    	cin >> x;
+    	cout << "\nValue of e^x using exp() built in function : " << exp(x) << endl;  //answer through function
 
-    for(int i = 1; i <= 100; i++) {
-      fact *= i;
-      exponent = exponent + pow(x, i) / fact;
-    }
-    cout << "\nValue of e^x through equation = " << exponent + 1 << endl;
+    	for(int i = 1; i <= 100; i++) {
+      		fact *= i;  //factorial finding
+      		exponent = exponent + pow(x, i) / fact;   //equation for finding exponent
+    	}
+    	cout << "\nValue of e^x through equation = " << exponent + 1 << endl;
 
-    cout << "\nDo you want to continue : ('Y' or 'N') " << endl;
-    cin >> choice; 
-  }while(choice == 'y' || choice == 'Y');
-  cout << "\nThank You!" << endl;
+    	cout << "\nDo you want to continue : ('Y' or 'N') " << endl;
+    	cin >> choice; 
+  	}while(choice == 'y' || choice == 'Y');
+  	cout << "\nThank You!" << endl;
 	cout << endl;
 	return 0;
 }
